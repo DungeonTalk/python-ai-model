4c03c2f8ce6a8370930115f1da642ba54b10aa9b
moonjun1 <puls55555@naver.com>
2025-08-19 16:52:56 +0900

    불필요한 내용 삭제

--- main_ai_only.py
+++ main_ai_only.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from langchain_postgres.vectorstores import PGVector
